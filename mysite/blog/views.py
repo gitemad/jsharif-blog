5615eda71a5480d48ac5cda994518bab79491626
Emad <sudani.emad@gmail.com>
2024-11-05 20:37:22 +0330

    feat: use slug in urls

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -7,7 +7,12 @@ def post_list(request):
 
     return render(request, 'blog/post/list.html', { 'posts': posts })
 
-def post_detail(request, id):
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+def post_detail(request, year, month, day, post_slug):
+    post = get_object_or_404(Post,
+                             slug=post_slug,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day,
+                             status=Post.Status.PUBLISHED)
     
     return render(request, 'blog/post/detail.html', { 'post': post })
