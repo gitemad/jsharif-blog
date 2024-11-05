42aa147f16170adff1fa695bd207d22eadcf85cb
Emad <sudani.emad@gmail.com>
2024-11-05 19:16:24 +0330

    refactor: use get_ibject_or_404 instead of handling it manually

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.http import Http404
 
 # Create your views here.
 def post_list(request):
@@ -9,9 +8,6 @@ def post_list(request):
     return render(request, 'blog/post/list.html', { 'posts': posts })
 
 def post_detail(request, id):
-    try:
-        post = Post.published.get(id=id)
-    except Post.DoesNotExist:
-        raise Http404('No post found.')
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
     
     return render(request, 'blog/post/detail.html', { 'post': post })
